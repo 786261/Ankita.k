46b387f17596e87a32c7825d57c17fb18c572e02
786261 <49330943+786261@users.noreply.github.com>
2019-04-06 12:04:04 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,23 @@
+hell0.c
+#include<stdio.h>
+void get_input(int*a,int*b)
+{
+    printf("enter the first number");
+    scanf("%d",a);
+    printf("enter the second number ");
+    scanf("%d",b);
+}
+int add2nos(int a,int b)
+{
+ int c;
+ a+b=c; 
+ return c;  
+}
+void get_output(int a,int b,int c)
+{
+    printf("%d+%d=%d");
+}
+int main()
+{
+
+}
